69a0f762e674e54ac47407ea075c8491bae5adf5
lili6 <34793278@qq.com>
2015-01-23 18:52:55 +0800

    initial

---
+++ src/main/java/lili/dp/simple/singleton/test.java
@@ -0,0 +1,7 @@
+package lili.dp.simple.singleton;
+
+/**
+ * Created by liguofang on 2015/1/23.
+ */
+public class test {
+}
---
+++ src/test/java/lili/dp/simple/test.java
@@ -0,0 +1,7 @@
+package lili.dp.simple;
+
+/**
+ * Created by liguofang on 2015/1/23.
+ */
+public class test {
+}
